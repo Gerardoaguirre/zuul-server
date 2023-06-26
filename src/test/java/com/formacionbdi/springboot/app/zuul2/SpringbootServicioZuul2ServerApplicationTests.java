4cad033f2d195bca45bec4b769dd1fe0ae44dab9
Gerardoaguirre <gecoaguirre@hotmail.com>
2023-06-25 20:12:45 -0600

    apigateway

---
+++ src/test/java/com/formacionbdi/springboot/app/zuul2/SpringbootServicioZuul2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.zuul2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioZuul2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
